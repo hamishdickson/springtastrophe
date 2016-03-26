d5cdd26234a86091af79f13ce039f0a56fc7347d
hamishdickson <hamish.dickson@gmail.com>
2016-03-26 20:11:32 +0000

    move asJson stuff

--- src/main/scala/springtastrophe/swagger/Swagger.scala
+++ src/main/scala/springtastrophe/swagger/Swagger.scala
@@ -1,5 +1,7 @@
 package springtastrophe.swagger
 
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+
 sealed trait Swagger
 
 final case class Body(swagger: String, info: Info, host: String, basePath: String, schemes: Option[List[String]], consumes: Option[List[String]], produces: Option[List[String]], paths: Option[Map[String, Path]], definitions: Option[Map[String, Definition]]) extends Swagger
@@ -8,5 +10,9 @@ final case class Contact(name: String, email: String, url: String) extends Swagg
 final case class Licence(name: String, url: String) extends Swagger
 
 // todo: Implement
-final case class Path() extends Swagger
-final case class Definition() extends Swagger
+final case class Path()
+final case class Definition()
+
+object Swagger {
+  def toJsonString(s: Body): String = s.asJson.noSpaces.toString
+}
--- src/test/scala/springtastrophe/swagger/SwaggerTest.scala
+++ src/test/scala/springtastrophe/swagger/SwaggerTest.scala
@@ -2,15 +2,13 @@ package springtastrophe.swagger
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
-
 class SwaggerTest extends FlatSpec with Matchers {
   "A Swagger definition" should "match something something something" in {
     val out: String = """{"swagger":"2.0","info":{"version":"1.0.0","title":"Swagger Petstore","description":"A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification","termsOfService":"http://swagger.io/terms/","contact":null,"licence":null},"host":"petstore.swagger.io","basePath":"/api","schemes":null,"consumes":null,"produces":null,"paths":null,"definitions":null}"""
 
     val info: Info = Info("1.0.0", "Swagger Petstore", "A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification", "http://swagger.io/terms/", None, None)
-    val b: Body = Body("2.0", info, "petstore.swagger.io", "/api", None, None, None, None, None)
+    val s: Body = Body("2.0", info, "petstore.swagger.io", "/api", None, None, None, None, None)
 
-    b.asJson.noSpaces should be (out)
+    Swagger.toJsonString(s) should be (out)
   }
 }
